07602112b5341e06a95ef38a3692c02f3634a7f5
NataliaSerdiuk <ivastudage@gmail.com>
2023-06-29 15:30:27 +0300

    mixins in my code

---
+++ newsite/newstories/utils.py
@@ -0,0 +1,21 @@
+from .models import *
+
+
+menu = [ {'title': "О сайте", 'url_name': 'about'},
+         {'title':" Добавить историю", 'url_name': 'add_page'},
+         {'title':"Обратная связь",'url_name': 'contact'},
+         {'title':"Войти на сайт", 'url_name': 'login'},
+         {'vote_selected': 0, 'url_name': 'no_vote'},
+         {'vote_selected': 1, 'url_name': 'best'},
+         {'posts_selected': 0, 'url_name': 'homepage'},
+]
+
+
+class DataMixin:
+    def get_user_context(self, **kwargs):
+        context = kwargs
+        user_menu = menu.copy()
+        if not self.request.user.is_authenticated:
+            user_menu.pop(1)
+        context['menu'] = user_menu
+        return context
--- newsite/newstories/views.py
+++ newsite/newstories/views.py
@@ -2,67 +2,59 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseNotFound
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import *
 from .models import *
+from .utils import *
 
 
-menu = [ {'title': "О сайте", 'url_name': 'about'},
-         {'title':" Добавить историю", 'url_name': 'add_page'},
-         {'title':"Обратная связь",'url_name': 'contact'},
-         {'title':"Войти на сайт", 'url_name': 'login'},
-]
 
-class StoriesHomepage(ListView):
+class StoriesHomepage(DataMixin, ListView):
     model = Stories
     template_name = 'newstories/index.html'
     context_object_name = 'posts'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = menu
-        context['title'] = 'Главная страница'
-        return context
+        c_def = self.get_user_context(title = "Главная страница",posts_selected=0)
+        return dict(list(context.items()) + list(c_def.items()))
+
 
     def get_queryset(self):
         return Stories.objects.filter(is_published = True)
 
-class StoriesBestRating(ListView):
+class StoriesBestRating(DataMixin, ListView):
     model = LikeDislike
     template_name = 'newstories/best.html'
     context_object_name = 'posts'
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = menu
-        context['title'] = 'Лучшие истории'
-        context['vote_selected'] = 1
-        return context
+        c_def = self.get_user_context(title="Лучшие истории", vote_selected=1)
+        return dict(list(context.items()) + list(c_def.items()))
     def get_queryset(self):
         return Stories.objects.annotate(rating=Sum('likedislike__vote')).order_by('-rating').filter(rating__gt=0)
 
-class StoriesWorstRating(ListView):
+class StoriesWorstRating(DataMixin, ListView):
     model = LikeDislike
     template_name = 'newstories/worst.html'
     context_object_name = 'posts'
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = menu
-        context['title'] = 'Скучные истории'
-        context['vote_selected'] = -1
-        return context
+        c_def = self.get_user_context(title="Скучные истории",vote_selected=-1)
+        return dict(list(context.items()) + list(c_def.items()))
     def get_queryset(self):
         return Stories.objects.annotate(rating=Sum('likedislike__vote')).order_by('-rating').filter(rating__lt=1)
 
-class StoriesNoRating(ListView):
+class StoriesNoRating(DataMixin, ListView):
     model = LikeDislike
     template_name = 'newstories/no_vote.html'
     context_object_name = 'posts'
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = menu
-        context['title'] = 'Неоцененные истории'
-        context['vote_selected'] = 0
-        return context
+        c_def = self.get_user_context(title="Неоцененные истории", vote_selected=0)
+        return dict(list(context.items()) + list(c_def.items()))
+
     def get_queryset(self):
         return Stories.objects.filter(likedislike__vote=None)
 
@@ -70,16 +62,16 @@ class StoriesNoRating(ListView):
 def about(request):
     return HttpResponse('О проекте')
 
-class AddPage(CreateView):
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'newstories/addpage.html'
     success_url = reverse_lazy('homepage')
+    login_url = '/admin/'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = menu
-        context['title'] = 'Добавить историю'
-        return context
+        c_def = self.get_user_context(title="Добавить историю")
+        return dict(list(context.items()) + list(c_def.items()))
 
 def contact(request):
     return HttpResponse('Обратная связь')
@@ -91,7 +83,7 @@ def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 
-class ShowPost(DetailView):
+class ShowPost(DataMixin, DetailView):
     model = Stories
     template_name = 'newstories/post.html'
     slug_url_kwarg = 'post_slug'
@@ -99,9 +91,8 @@ class ShowPost(DetailView):
 
     def get_context_data(self, *, object_list = None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = menu
-        context['title'] = context['post']
-        return context
+        c_def = self.get_user_context(title=context['post'])
+        return dict(list(context.items()) + list(c_def.items()))
 
 
 
