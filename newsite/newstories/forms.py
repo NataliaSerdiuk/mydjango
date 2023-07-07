b26518ddd8675a85930a7903ba548afaffa6bbf8
NataliaSerdiuk <ivastudage@gmail.com>
2023-07-07 15:12:16 +0300

    registration

--- newsite/newstories/forms.py
+++ newsite/newstories/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
 
 from .models import *
 
@@ -10,6 +11,7 @@ class AddPostForm(forms.ModelForm):
     class Meta:
         model = Stories
         fields = ['title', 'slug', 'content', 'photo', 'is_published']
+
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-input'}),
             'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
@@ -47,3 +49,8 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name', 'username','email', 'password1', 'password2')
+
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
--- newsite/newstories/views.py
+++ newsite/newstories/views.py
@@ -4,12 +4,13 @@ from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout, login
 
 
 from .forms import *
 from .models import *
 from .utils import *
-
 
 
 class StoriesHomepage(DataMixin, ListView):
@@ -22,36 +23,43 @@ class StoriesHomepage(DataMixin, ListView):
         c_def = self.get_user_context(title = "Главная страница",posts_selected=0)
         return dict(list(context.items()) + list(c_def.items()))
 
-
     def get_queryset(self):
         return Stories.objects.filter(is_published = True)
+
 
 class StoriesBestRating(DataMixin, ListView):
     model = LikeDislike
     template_name = 'newstories/best.html'
     context_object_name = 'posts'
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title="Лучшие истории", vote_selected=1)
         return dict(list(context.items()) + list(c_def.items()))
+
     def get_queryset(self):
         return Stories.objects.annotate(rating=Sum('likedislike__vote')).order_by('-rating').filter(rating__gt=0)
+
 
 class StoriesWorstRating(DataMixin, ListView):
     model = LikeDislike
     template_name = 'newstories/worst.html'
     context_object_name = 'posts'
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title="Скучные истории",vote_selected=-1)
         return dict(list(context.items()) + list(c_def.items()))
+
     def get_queryset(self):
         return Stories.objects.annotate(rating=Sum('likedislike__vote')).order_by('-rating').filter(rating__lt=1)
+
 
 class StoriesNoRating(DataMixin, ListView):
     model = LikeDislike
     template_name = 'newstories/no_vote.html'
     context_object_name = 'posts'
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title="Неоцененные истории", vote_selected=0)
@@ -60,9 +68,40 @@ class StoriesNoRating(DataMixin, ListView):
     def get_queryset(self):
         return Stories.objects.filter(likedislike__vote=None)
 
+def get_like_dislike(request, post_slug):
+    post = get_object_or_404(Stories, slug=post_slug)
+    existing_vote = None
+
+    if request.method == 'POST' and request.user.is_authenticated:
+        vote = request.POST.get('vote')
+        existing_vote = LikeDislike.objects.filter(story=post, author=request.user).first()
+
+        if not existing_vote:
+            if vote == 'like':
+                LikeDislike.objects.create(vote=LikeDislike.LIKE, story=post, author=request.user)
+            elif vote == 'dislike':
+                LikeDislike.objects.create(vote=LikeDislike.DISLIKE, story=post, author=request.user)
+            return redirect('post', post_slug=post.slug)
+        if existing_vote.vote == 1:
+            if vote == 'dislike':
+                existing_vote.delete()
+                LikeDislike.objects.create(vote=LikeDislike.DISLIKE, story=post, author=request.user)
+            return redirect('post', post_slug=post.slug)
+        if existing_vote.vote == -1:
+            if vote == 'like':
+                existing_vote.delete()
+                LikeDislike.objects.create(vote=LikeDislike.LIKE, story=post, author=request.user)
+            return redirect('post', post_slug=post.slug)
+
+    context = {
+        'post': post,
+        'existing_vote': existing_vote,
+    }
+    return render(request, 'newstories/post.html', context)
 
 def about(request):
     return HttpResponse('О проекте')
+
 
 class AddPage(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
@@ -75,11 +114,12 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
         c_def = self.get_user_context(title="Добавить историю")
         return dict(list(context.items()) + list(c_def.items()))
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 def contact(request):
     return HttpResponse('Обратная связь')
-
-def login(request):
-    return HttpResponse('Авторизация')
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
@@ -94,6 +134,10 @@ class ShowPost(DataMixin, DetailView):
     def get_context_data(self, *, object_list = None, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title=context['post'])
+
+        rating = self.object.get_rating()
+        context['rating'] = rating
+
         return dict(list(context.items()) + list(c_def.items()))
 
 
@@ -107,3 +151,23 @@ class RegisterUser(DataMixin, CreateView):
         c_def = self.get_user_context(title="Регистрация")
         return dict(list(context.items()) + list(c_def.items()))
 
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('homepage')
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'newstories/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('homepage')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
